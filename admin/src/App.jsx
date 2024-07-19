44b69376575972c30139478c121a3114320e2dab
G Shiva <96604363+shivajr78@users.noreply.github.com>
2024-07-19 11:41:26 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
           <Route path="/add" element={<Add url={url}/>}/>
           <Route path="/list" element={<List url={url}/>}/>
           <Route path="/orders" element={<Orders url={url}/>}/>
+          <Route path="*" element={<List url={url}/>} />
         </Routes>
       </div>
     </div>
