f000073a98e69143004b5ef5ac4fb0a8537b7c43
shivajr78 <beingshiva78@gmail.com>
2024-07-31 11:00:18 +0530

    logo change

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
-
-
+import { assets } from "../../assets/assets";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div className='footer' id='footer'>
+    <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-            <img className='tomatologofooter' src={assets.logo} alt="" />
-            <p>This website is just for my portfolio, it's not a real website.</p>
+            <img className="logo4" src={assets.logo4} alt="" />
+            <p>Foodie Restaurant and Cafe is a vibrant culinary destination, blending diverse flavors for a delightful dining experience in an inviting ambiance.</p>
             <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-              <img src={assets.twitter_icon} alt="" />
-              <img src={assets.linkedin_icon} alt="" />
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
             </div>
         </div>
         <div className="footer-content-center">
             <h2>COMPANY</h2>
             <ul>
-              <li>Home</li>
-              <li>About us</li>
-              <li>Delivery</li>
-              <li>Privacy Policy</li>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
             </ul>
         </div>
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-              <li>+1-212-456-7890</li>
-              <li>contact@tomato.com</li>
+                <li>+91-9813682478</li>
+                <li>contact@foodie.com</li>
             </ul>
         </div>
+        
       </div>
-      <hr/>
-      <p className='footer-copyright'>Copyright 2024 © Tomato.com - All rights reserved.</p>
+      <hr />
+      <p className="footer-copyright"> By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2018-2024 © Foodie™ Ltd. All rights reserved.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
